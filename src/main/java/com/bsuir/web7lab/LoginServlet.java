6fadffd686d2263d04d6ac9bc1f94f561f495ff0
VladikKorotkov <mr.vlad.korotkov@mail.ru>
2023-11-25 21:26:30 +0300

    add LoginServlet

---
+++ src/main/java/com/bsuir/web7lab/LoginServlet.java
@@ -0,0 +1,30 @@
+package com.bsuir.web7lab;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Good morning </title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println(" First Servlet");
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
